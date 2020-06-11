8a92efdbf284e9de1c0ac3a8da57876e8b8105fc
vdsalex <alexandre.vandensteen@gmail.com>
2020-06-11 22:39:58 +0200

    initial commit, add first green test (config test)

---
+++ src/test/java/lcdlv/ing/kata/IngKataTest.java
@@ -0,0 +1,13 @@
+package lcdlv.ing.kata;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class IngKataTest {
+
+    @Test
+    void test_configuration() {
+        assertThat(true).isTrue();
+    }
+}
